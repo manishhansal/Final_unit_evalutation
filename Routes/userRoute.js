d1fe0c89393de57fc238d15aadfb5e2090daf827
Manish Kumar <manishhansalmeh@gmail.com>
2022-05-07 16:49:59 +0530

    update

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,10 @@
 // const router = require('express').Router();
 const express = require('express');
 const app = express();
-const userModel = require('../Models/userModel');
+const userModel = require('../Models/userModel')
 const bookModel = require('../Models/bookModel');
-const commentModel = require('../Models/commentModel')
+const commentModel = require('../Models/commentModel');
+
 app.use(express.json());
 
 app.post('/register', async(req, res, next) => {
@@ -20,8 +21,19 @@ app.post('/register', async(req, res, next) => {
 
 });
 
-app.post('/userLogin', (req, res, next) => {
-    res.send("User login route.")
+app.post('/userLogin', async(req, res, next) => {
+    // res.send("User login route.")
+    try {
+    
+        // let email = req.body;
+        let response = await userModel.find({email: req.body});
+        console.log(response);
+        res.json(response);
+
+    } catch (error) {
+        res.json(error);
+    }
+
 });
 
 app.post('/createBook', async(req, res, next) => {
@@ -50,8 +62,17 @@ app.post('/createComment', async(req, res, next) => {
     }
 });
 
-app.get('/viewPosts', (req, res, next) => {
-    res.send("View posts route.");
+app.get('/viewPosts', async(req, res, next) => {
+    // res.send("View posts route.");
+    try {
+    
+        let response = await userModel.find({});
+        res.json(response);
+
+    } catch (error) {
+        res.json(error);
+    }
+
 });
 
 
